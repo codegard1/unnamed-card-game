790135c8e6f1df293bd4499b157ab711486b4c0b
Chris Odegard <c.odegard@gmail.com>
2026-01-18 19:40:58 -0500

    ensure CardTheme is exported first to avoid circular dependency issues

--- src/cardStyles/index.ts
+++ src/cardStyles/index.ts
@@ -1,10 +1,11 @@
+export * from './CardTheme'; // Ensure this is always exported before CARD_THEMES to avoid circular dependency issues
+
 export * from './BackgroundType';
 export * from './CARD_STYLE_PRESETS';
 export * from './CARD_THEMES';
 export * from './CardBackStyle';
 export * from './CardFrontStyle';
 export * from './CardStyleConfig';
-export * from './CardTheme';
 export * from './GradientConfig';
 export * from './SymbolStyle';
 export * from './ThemeConfig';
